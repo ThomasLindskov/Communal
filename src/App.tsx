0f3ebe17d9945422ee2fda41e1fc7ca69569c6d2
thch@it-kartellet.dk <thch19ab@student.cbs.dk>
2022-10-30 11:36:23 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import RoutesTable from "./routes/RoutesTable";
+
 
 function App() {
   return (
-    <div className="App">
+      <RoutesTable/>
+    /*<div className="App">
       <header className="App-header">
         <p>Mount styled components here!</p>
       </header>
-    </div>
+    </div>*/
   );
 }
 
