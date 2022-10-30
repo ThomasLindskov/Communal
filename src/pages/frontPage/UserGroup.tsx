4a726b42647a9c6d4a0d2fa75f0034cbf2accbfb
Niklas <nlb@it-kartellet.dk>
2022-10-30 14:17:34 +0100

    UserGroup grid-template

--- src/pages/frontPage/UserGroup.tsx
+++ src/pages/frontPage/UserGroup.tsx
@@ -1,42 +1,41 @@
 import React from 'react'
+import styled from "styled-components"
 
-const style = {
-    container: {
-        flex: 1,
-        backgroundColor: '#081F2E',
-        width: '1000px',
+const Container = styled.div`
+background-color: ${({ theme }) => theme.colors.primary};
+display: grid;
+height: 500px;
+width: 500px;
+grid-template-areas:
+". . x x . . . . . ."
+". . x x . . . . . ."
+". . . . a a a . . ."
+". . . . a a a . . ."
+"y y . . . . . . z z"
+"y y . . . . . . z z"
+". . . b b . c c . ."
+". . . b b . c c . ."
+". . q q . . . w w ."
+". . q q . . . w w .";
+`;
+const Icon = styled.img`
+width: 80%;
+height: 80%;
+`;
 
-    },
-    icon: {
-        width: '10%',
-        height: '10%',
-        margin: '10px',
-    },
-    bubble: {
-        margin: '20px',
-    },
-    left: {
-        transform: 'rotate(90deg)'
-    },
-    top: {
-        transform: 'rotate(180deg)',
-        marginLeft: '100px',
-    },
-    right: {
-        transform: 'rotate(-90deg)'
-    }
-}
+export const UserGroup = () => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '50px' }}>
+        <Container>
+            <Icon style={{ gridArea: 'x', transform: 'rotate(180deg)' }} src="/icons/users/Group 1.svg" alt="Adult Male Icon" />
+            <Icon style={{ gridArea: 'y', transform: 'rotate(90deg)' }} src="/icons/users/Group 9.svg" alt="Young Male Icon" />
+            <Icon style={{ gridArea: 'z', transform: 'rotate(-90deg)' }} src="/icons/users/Group 7.svg" alt="Young Female Icon" />
+            <Icon style={{ gridArea: 'q' }} src="/icons/users/Group 10.svg" alt="Adult Female Icon" />
+            <Icon style={{ gridArea: 'w' }} src="/icons/users/Group 12.svg" alt="Adult Female Icon" />
+            <Icon style={{ gridArea: 'a' }} src="/icons/bubbles/SpeechBubble1.svg" alt="Speech Bubble1" />
+            <Icon style={{ gridArea: 'b' }} src="/icons/bubbles/SpeechBubble2.svg" alt="Speech Bubble2" />
+            <Icon style={{ gridArea: 'c' }} src="/icons/bubbles/SpeechBubble3.svg" alt="Speech Bubble3" />
+        </Container>
+    </div>
 
-export default function UserGroup() {
-    return (
-        <div style={style.container}>
-            <img style={{ ...style.icon, ...style.left }} src="/icons/users/Group 1.svg" alt="Adult Male Icon" />
-            <img style={{ ...style.icon, ...style.right }} src="/icons/users/Group 9.svg" alt="Young Male Icon" />
-            <img style={{ ...style.icon, ...style.top }} src="/icons/users/Group 7.svg" alt="Young Female Icon" />
-            <img style={style.icon} src="/icons/users/Group 10.svg" alt="Adult Female Icon" />
-            <img src="/icons/bubbles/SpeechBubble1.svg" alt="Speech Bubble1" />
-            <img src="/icons/bubbles/SpeechBubble2.svg" alt="Speech Bubble2" />
-            <img src="/icons/bubbles/SpeechBubble3.svg" alt="Speech Bubble3" />
-        </div>
-    )
-}
+
+)
