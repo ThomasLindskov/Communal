a09622039a51dfea2e7f5bb97b90d4e270f34ff8
Philip van Burleigh <pvburleigh@gmail.com>
2022-11-08 15:48:26 +0100

    correct parse version added to package.json + minor css fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,6 @@ export const Card = styled.div<{ width: string }>`
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: ${({ theme }) => theme.utils.borderRadius};
   padding: ${({ theme }) => theme.padding.large};
-  filter: drop-shadow(${({ theme }) => theme.utils.dropShadow});
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.padding.medium};
