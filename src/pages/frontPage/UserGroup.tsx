b9fe98e27bfe86c8040ad699df1e2848573345cb
NiklasLB <nlyk@itu.dk>
2022-11-15 13:38:39 +0100

    Revert "Icons added, need to add props to change position"
    This reverts commit 5a2bccc36b7f2b29ae88087ee7f4c224ee4b491d.

--- src/pages/frontPage/UserGroup.tsx
+++ src/pages/frontPage/UserGroup.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { start } from 'repl';
 import styled from "styled-components"
+import Icon from '../../components/Icon';
 import { UserIconContainer } from './UserIconContainer';
 
 const Container = styled.div`
