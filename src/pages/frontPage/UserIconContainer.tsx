1fbe81973d019ae1337152f1d9eff8bcff1f0348
thch@it-kartellet.dk <thch19ab@student.cbs.dk>
2022-11-19 13:36:20 +0100

    The new frontPage

--- src/pages/frontPage/UserIconContainer.tsx
+++ src/pages/frontPage/UserIconContainer.tsx
@@ -1,33 +1,45 @@
 import React from 'react'
-import { HTMLInputTypeAttribute } from "react";
 import styled from "styled-components";
-import Icon from '../../components/Icon';
+import {Icon} from '../../components/Icon';
+import data from './groups.json';
 
-interface IIconContainerProps {
-    gridArea: string
 
-}
-
-const IconContainer = styled.div<IIconContainerProps>`
-    grid-area: ${(props) => props.gridArea};
+const IconContainer = styled.div`
     display: flex;
     overflow: hidden;}
     `
 
-
+let random = data.sort(() => .5 - Math.random()).slice(0,5)
 
 export const UserIconContainer = ({
-  src,
-  gridArea,
-  style,
-  iconStyle
+  number,
+  className,
+  setTestimonial
 }: {
-  src: string;
-  gridArea: string;
-  style?: React.CSSProperties;
-  iconStyle?: React.CSSProperties
-}) => (
-    <IconContainer gridArea={gridArea} style={{ ...style }}>
-        <Icon src={src} style={{...iconStyle}}/>
-    </IconContainer>
-);
+  number: number;
+  className: string;
+  setTestimonial: Function;
+}) => {
+  const handleMouseEnter = () => {
+    setTestimonial(makeQuote(random[number].testimonial, random[number].madeBy));
+  };
+  const handleMouseLeave = () => {
+    setTestimonial("Hover over us, to hear our experiences");
+  };
+
+  return (
+      <IconContainer className={className}>
+          <Icon src={random[number].src} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+      </IconContainer>
+  );
+}
+
+function makeQuote(testimonial:String, madeBy:String) {
+  return(<blockquote>
+    <p>{testimonial}</p>
+    
+    <cite>— {madeBy}</cite>
+    </blockquote>
+    )
+  
+}
