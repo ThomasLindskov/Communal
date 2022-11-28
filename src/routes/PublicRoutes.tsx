9d75e5cf68e875fb97d714b65271fd0ad0b5498d
NiklasLB <nlyk@itu.dk>
2022-11-28 16:10:36 +0100

    Preventing users from accessing private routes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -3,12 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthQuery } from "src/hooks/useAuthQuery";
 export const PublicRoutes = () => {
   const { data, error, loading } = useAuthQuery();
-  const logout = localStorage.getItem("loggingOut");
-
-  if (logout) {
-    localStorage.clear();
-    return <Outlet />;
-  }
 
   if (data) {
     localStorage.setItem("currentUser", data.viewer.user.id);
