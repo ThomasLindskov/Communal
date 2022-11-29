93875bfed92c6b877ab358f67a8be9b386030818
emilarenthertz <73878625+emilarenthertz@users.noreply.github.com>
2022-11-29 14:58:37 +0100

    WIP: UploadImageForm

--- src/pages/editProfilePage/UploadImageForm.tsx
+++ src/pages/editProfilePage/UploadImageForm.tsx
@@ -13,132 +13,126 @@ import {
   IUploadImageInput,
 } from "./validationSchemas/UploadImage";
 import { useUploadImageMutation } from "src/hooks/useUploadImageMutation";
-
+import { uploadImage } from "src/parse/uploadImage";
 
 export const UploadImageForm = () => {
   //const [images, setImages] = useState([]);
 
-
-   const {
-    uploadImage,
-    loading: submitLoading,
-    error: submitError,
-  } = useUploadImageMutation(); 
   const {
     register,
     formState: { errors, isDirty },
     // setValue,
     handleSubmit,
-  } = useForm<IUploadImageInput>({
-    resolver: yupResolver(uploadImageSchema),
-  });
+  } = useForm<IUploadImageInput>();
+  // resolver: yupResolver(uploadImageSchema),
 
-
-  //const { loading, error } = useProfileSettingsQuery(setValue);
-
-
-  const onSubmit: SubmitHandler<IUploadImageInput> = (inputData) => {
+  const onSubmit: SubmitHandler<IUploadImageInput> = async (inputData) => {
     if (!isDirty) {
       toast("No image selected");
       return;
     }
 
-    const { image } = inputData;
-    const input = {
-        id: localStorage.getItem("currentUser"),
-        fields: {
-          image
-        },
-      };
-      
-      uploadImage({
-        variables: { input },
-        onCompleted: () => {
-          if (submitError) {
-            toast(submitError.message);
-          } else {
-            toast.success("Profile updated successfully");
-          }
-        },
-      });
-    };
-      
+    const image = inputData.image[0];
+    const success = await uploadImage(image);
 
-    // if (loading) return null;
-    // if (error) return <p>{`Error :${error.message}`}</p>;
-
-    // async function retriveImages() {
-    //   const query = new Parse.Query("Image");
-    //   let results = await query.find();
-
-    //   for(let i = 0; i < results.length; i++) {
-    //     let object = results[i];
-    //     console.log(object.id + ' - ' + object.get('image') + " ") + JSON.stringify(object.get('image'));
-        
-    //   }
-    // }
-
-    // retriveImages();
-
-
-  const fetchImage = gql`
-  query GetUser($id: ID!) {
-    user(id: $id) {
-      image
+    if (success) {
+      toast.success("Image uploaded successfully");
+    } else {
+      toast.error("Image upload failed");
     }
-  }
-`;
 
-console.log(fetchImage);
+    // const { image } = inputData;
+    // const input = {
+    //     id: localStorage.getItem("currentUser"),
+    //     fields: {
+    //       image
+    //     },
+    //   };
 
-// useEffect(() => {
-//   const loadedimages = async () => {
-//     let query = new Parse.Query("Image");
-//     const results = await query.find();
-//     setImages(results);
-// };
-// loadedimages();
-//  }, []);
+    // uploadImage({
+    //   variables: { input },
+    //   onCompleted: () => {
+    //     if (submitError) {
+    //       toast(submitError.message);
+    //     } else {
+    //       toast.success("Profile updated successfully");
+    //     }
+    //   },
+    // });
+  };
 
-  
+  // if (loading) return null;
+  // if (error) return <p>{`Error :${error.message}`}</p>;
+
+  // async function retriveImages() {
+  //   const query = new Parse.Query("Image");
+  //   let results = await query.find();
+
+  //   for(let i = 0; i < results.length; i++) {
+  //     let object = results[i];
+  //     console.log(object.id + ' - ' + object.get('image') + " ") + JSON.stringify(object.get('image'));
+
+  //   }
+  // }
+
+  // retriveImages();
+
+  // const fetchImage = gql`
+  //   query GetUser($id: ID!) {
+  //     user(id: $id) {
+  //       image
+  //     }
+  //   }
+  // `;
+
+  // console.log(fetchImage);
+
+  // useEffect(() => {
+  //   const loadedimages = async () => {
+  //     let query = new Parse.Query("Image");
+  //     const results = await query.find();
+  //     setImages(results);
+  // };
+  // loadedimages();
+  //  }, []);
+
   return (
     <form
-    onSubmit={handleSubmit(onSubmit)}
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: theme.flexGap.medium,
-      alignItems: "center",
-    }}
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.flexGap.medium,
+        alignItems: "center",
+      }}
     >
-            <Card width="200px">
-      <div style={{ width: "100%" }}>
-            <CardTitle children="Edit Profile" />
-          </div>
-          <div>
-            <p>this is where an image should be displayed</p>
-            
-          </div>
-          <div>
-            <img src="" alt="" />
-            <p>this is where u can upload new image</p>
-            <InputField
-              label="Image"
-              id="image"
-              type="file"
-              placeholder="Image"
-              style={{ div: { width: "100%" } }}
-              register={register("image")}
-              errorMessage={errors.image?.message}
-            />
-          </div>
-          <Button
-            color={theme.colors.cta}
-            type="submit"
-             disabled={submitLoading}
-           >
-            Upload Image
-          </Button>
+      <Card width="200px">
+        <div style={{ width: "100%" }}>
+          <CardTitle children="Edit Profile" />
+        </div>
+        <div>
+          <p>this is where an image should be displayed</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>this is where u can upload new image</p>
+          <InputField
+            label="Image"
+            id="image"
+            type="file"
+            placeholder="Image"
+            style={{ div: { width: "100%" } }}
+            register={register("image")}
+            errorMessage={errors.image?.message}
+          />
+        </div>
+        <Button
+          color={theme.colors.cta}
+          type="submit"
+          //  disabled={submitLoading}
+        >
+          Upload Image
+        </Button>
       </Card>
       <Toaster position="bottom-center" />
     </form>
